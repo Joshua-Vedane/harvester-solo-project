ddef8e1b7fc1c6fbf9559949f879048642dd6438
Joshua Vedane <jvedane@gmail.com>
2021-02-28 12:21:55 -0600

    updated readme and database instructions

--- README.md
+++ README.md
@@ -119,3 +119,60 @@ This code is also heavily commented. We recommend reading through the comments, 
 ## Update Documentation
 
 Customize this ReadMe and the code comments in this project to read less like a starter repo and more like a project. Here is an example: https://gist.github.com/PurpleBooth/109311bb0361f32d87a2
+
+
+# Harvester Solo Project
+
+Harvester is a mobile application designed to help independent construction contractors / any small business to manage their project from their phone throughout the day. Working with many such contractors from years of painting houses, I noticed that the last thing they want to do after sweating all day is to input data to an excel sheet. Harvester tracks expenses for individual projects for easier analysis and management. 
+
+
+## Getting Started
+
+See deployed version at https://secret-stream-65008.herokuapp.com/#/home
+
+If you wish to spin this project up locally: 
+1. Install node if not installed. 
+2. Open up the file in your editor of choice and run 'npm install' in the terminal. 
+3. Create a PostgreSQL database from the database.sql file in this project. Also, ensure your database matches the database configuration found in './server/modules/pool.js'
+4. Type 'npm run server' in the terminal
+5. Open another terminal in the same project and type 'npm run client'
+6. Go to 'localhost:3000' in your browser to use the app. Upon starting the client, the app should automatically open.
+
+
+## Built With
+React
+Redux / Redux-Saga
+Node
+Express
+postgreSQL
+Material UI
+passport
+
+### Future Features
+1. There are some large components in this project, namely the forms. Will break those out into separate components .
+2. Store hours worked and create admin/employee views. To accomplish this, I would need to refactor the database which in turn would require changing up data handling app wide. This would be a really large task. I'm thinking it would take adding a column for access levels on the user table. For wages, that would be a separate table. But it would need to link up with the users, and project_expenses table at a minimum. If you have ideas on how to accomplish this, I'm all ears! 
+3. Allow users to add documentation to each project. i.e. contracts, licenses, permits, and notes from verbal communication with clients. 
+4. Add a search feature to the dashboard view.
+5. Add a sort feature to the table in project details view. 
+6. Add pagination to the dashboard. Loading more than 5~7 projects takes some time, especially if there is an image attached to them. 
+7. Speaking of images, let's do some image upload ability. It would be cool to be able to upload a local image from your phone. I'm not sure exactly how that works but It'll be fun learning all about it!
+
+
+### Acknowledgments 
+* Thank you to the instructors at Prime Digital Academy for helping me out on this project. Especially on the scoping aspect and helping me to know how much I can accomplish in two weeks. 
+* Thank you to Brian Codex for inspiration for the Navigation menu. 
+## Authors
+
+* **Billie Thompson** - *Initial work* - [PurpleBooth](https://github.com/PurpleBooth)
+
+See also the list of [contributors](https://github.com/your/project/contributors) who participated in this project.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* Hat tip to anyone whose code was used
+* Inspiration
+* etc
--- database.sql
+++ database.sql
@@ -1,76 +1,82 @@
 -- CREATE DATABASE "harvester"
 
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    user_name character varying(40) NOT NULL UNIQUE,
+    password character varying(1000) NOT NULL,
+    hourly_rate numeric(18,2) NOT NULL
+);
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+CREATE TABLE projects (
+    id SERIAL PRIMARY KEY,
+    address_1 character varying(60) NOT NULL,
+    address_2 character varying(60) NOT NULL,
+    bid numeric(18,2) NOT NULL DEFAULT '0'::numeric,
+    start_date character varying(20) NOT NULL,
+    image character varying(200) DEFAULT 'images/placeholder.png'::character varying
+);
 
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    category_name character varying(255) NOT NULL
+);
 
-CREATE TABLE "users" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"user_name" varchar(40) NOT NULL UNIQUE,
-	"password" varchar(40) NOT NULL,
-	"hourly_rate" DECIMAL(18,2) NOT NULL
+CREATE TABLE user_projects (
+    id SERIAL PRIMARY KEY,
+    user_id integer NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    project_id integer NOT NULL REFERENCES projects(id) ON DELETE CASCADE
+);
+
+CREATE TABLE project_expenses (
+    id SERIAL PRIMARY KEY,
+    project_id integer REFERENCES projects(id) ON DELETE CASCADE,
+    category_id integer REFERENCES categories(id) ON DELETE CASCADE,
+    description character varying(125) NOT NULL,
+    date character varying(20) NOT NULL,
+    total numeric(18,2) NOT NULL
 );
 
 
-CREATE TABLE "projects" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"address_1" varchar(60) NOT NULL,
-	"address_2" varchar(60) NOT NULL,
-	"bid" DECIMAL(18,2) NOT NULL DEFAULT '0',
-	"start_date" VARCHAR(20) NOT NULL,
-	"image" VARCHAR(200) DEFAULT ''
-);
+--Dummy Data 
+--Create at least two users through the application itself before continuing
 
-
-
-CREATE TABLE "categories" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"category_name" varchar(255) NOT NULL
-);
-
-CREATE TABLE "project_expenses" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"project_id" INT REFERENCES "projects",
-	"category_id" INT REFERENCES "categories",
-	"description" varchar(125) NOT NULL,
-	"date" VARCHAR(20) NOT NULL,
-	"total" DECIMAL (18,2) NOT NULL
-);
-
-
-CREATE TABLE "user_projects" (
-	"id" SERIAL PRIMARY KEY NOT NULL,
-	"user_id" INT REFERENCES "users" NOT NULL,
-	"project_id" INT REFERENCES "projects" NOT NULL
-	
-);
-
--- create users
-INSERT INTO "users" ("user_name", "password", "hourly_rate")
+INSERT INTO "categories"("id","category_name")
 VALUES
-('joshua', 'hello', '15'),
-('mike', 'taco', '20');
+(1,'Equipment'),
+(2,'Materials'),
+(3,'Permit'),
+(4,'Wage');
 
---create categories
-INSERT INTO "categories" ("category_name")
-VALUES 
-('Equipment'),
-('Materials'),
-('Permit'),
-('Wage');
+INSERT INTO "projects"("id","address_1","address_2","bid","start_date","image")
+VALUES
+(1,'1735 Dayton','Saint Paul, MN 55104',15250,'05/16/2020','images/dayton_close.jpg'),
+(18,'1755 Ashland Ave.','Saint Paul, MN 55104',12500,'07/20/2020','images/ashland.jpg'),
+(19,'2922 Dorman Ave.','Minneapolis, MN 55406',7900,'08/16/2020','images/dayton_exterior.jpg'),
+(20,'214 Woodlawn Ave.','Saint Paul, MN 55105',8600,'10/02/2020','images/overhang.jpg'),
+(21,'2217 Summit Ave.','Saint Paul, MN 55104',14500,'10/21/2020','images/summit_french.jpg'),
+(22,'1251 W Como Ave.','Saint Paul, MN 55117',9850,'11/08/2020','images/summit_entrance.jpg'),
+(27,'1645 Laurel Ave ','Saint Paul, MN 55104',14500,'03/01/2021','images/placeholder.png');
 
---create new project
-INSERT INTO "projects" ("address_1", "address_2", "bid", "start_date")
-VALUES 
-('1767 Dayton', 'Saint Paul, MN 55105', '10000', '09/16/20');
+-- expenses for project id #22 (1251 W Como Ave.)
+INSERT INTO "public"."project_expenses"("id","project_id","category_id","description","date","total")
+VALUES
+(17,22,1,'bushes & drops','11/08/2020',125),
+(18,22,2,'primer','11/08/2020',225),
+(19,22,2,'chaulk','11/08/2020',22.5),
+(20,22,4,'joshua','11/08/2020',141.75),
+(21,22,2,'glasing','11/09/2020',25.48),
+(22,22,1,'torch&gloves','11/09/2020',36.39),
+(23,22,4,'joshua','11/09/2020',162),
+(24,22,4,'joshua','11/10/2020',162),
+(25,22,4,'mike','11/11/2020',198),
+(26,22,4,'joshua','11/11/2020',182.25),
+(27,22,2,'paint','11/12/2020',192.48),
+(28,22,4,'joshua','11/12/2020',162),
+(29,22,4,'mike','11/12/2020',176),
+(30,22,2,'paint and rags','11/13/2020',98.47),
+(31,22,4,'joshua','11/13/2020',141.75),
+(32,22,4,'mike','11/13/2020',154),
+(39,22,2,'Paint & brushes','02/22/2021',128),
+(40,22,4,'joshua','02/22/2021',162);
 
--- add expense to project
-INSERT INTO "public"."project_expenses"("project_id", "category_id", "description", "date", "total") VALUES(1, 1, 'brushes', '09/19/20', 50) RETURNING "id", "project_id", "category_id", "description", "date", "total";
-
--- Add user to project
-INSERT INTO "public"."user_projects"("user_id", "project_id") VALUES(1, 1) RETURNING "id", "user_id", "project_id";
---
-
+-- Add users to a project, ideally 1251 W Como as it is the only one with expenses linked to it through the application. 
